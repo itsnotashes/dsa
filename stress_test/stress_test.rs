9fb7572e5c2cd68f36b8c9e20160e5bebe97e0d9
itsnotashes <itsnotamit@gmail.com>
2022-09-12 20:36:35 +0530

    stress test on go floating point numers

---
+++ stress_test/stress_test.rs
@@ -0,0 +1,9 @@
+// Some weird results going on with rust. Where if x < 0.1 and x is incremented by 0.01, random text is printed 11 times. But for x < 0.01 when x is incremented by 0.001, random text is only printed 10 times.
+
+fn main(){
+    let mut x: f32 = 0.0;
+    while x < 0.1 {
+        println!("Random text");
+        x += 0.01;
+    }
+}
